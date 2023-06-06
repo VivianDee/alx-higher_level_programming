d04f7ee7c182f6fb4abfe7b1271ce62f9b07a33c
Vivian Dagbue <vdagbue@gmail.com>
2023-06-06 05:58:16 -0700

    Fix: pycodestyle

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,6 @@ def uppercase(str):
     for letter in str:
         num = ord(letter)
         if num > 96 and num < 123:
-           num = num - 32 
+            num = num - 32
         print("{}".format(chr(num)), end="")
     print()
