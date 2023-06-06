496009a41d5dbc2cdd42f7c23da3be4452669563
Vivian Dagbue <vdagbue@gmail.com>
2023-06-06 05:54:01 -0700

    Fix: Numbet of print functions

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,8 +2,8 @@
 for num1 in range(0, 9):
     for num2 in range(1, 10):
         if num1 == 8 and num2 == 9:
-            print((num1 * 10) + num2)
+            print("{}".format((num1 * 10) + num2))
         elif num1 == num2 or num1 > num2:
             num2 += 1
         else:
-            print((num1 * 10) + num2, end=", ")
+            print("{}, ".format((num1 * 10) + num2), end="")
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,6 @@ def uppercase(str):
     for letter in str:
         num = ord(letter)
         if num > 96 and num < 123:
-            print("{}".format(chr(num - 32)), end="")
-        else:
-            print("{}".format(letter), end="")
+           num = num - 32 
+        print("{}".format(chr(num)), end="")
     print()
