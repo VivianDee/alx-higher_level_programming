70f08e39c2f272e6e67df3a3acdb14c8449c61c9
Vivian Dagbue <vdagbue@gmail.com>
2023-06-07 06:56:30 -0700

    Fix: Empty list and list containing one node

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,17 +1,29 @@
 #include "lists.h"
 
+/**
+ * check_cycle - Checks if a singly linked list has a cycle in it
+ * @list: A pointer to the head of the list
+ *
+ * Return:
+ *-(0) if there is no cycle
+ *-(1) if there i a cycle
+ */
+
 int check_cycle(listint_t *list)
 {
 	int i;
 	listint_t *temp = NULL;
 
+	if (list == NULL || list->next == NULL)
+	{
+		return (0);
+	}
 	temp = list;
 	for (i = 0; temp != NULL; i++)
 	{
-		if (temp < temp->next)
+		if (temp < temp->next && temp->next != NULL)
 			return (1);
-		else
-			temp = temp->next;
+		temp = temp->next;
 	}
 	return (0);
 }
