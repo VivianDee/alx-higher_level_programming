4ca2533c19cc96fa7e9e137736a02c74ea5e24f8
Vivian Dagbue <vdagbue@gmail.com>
2023-06-08 05:02:33 -0700

    Style: Removed tabs used for indentation

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-	import sys
-	length = len(sys.argv)
-	length = length - 1
-	if length == 0:
-		print("0 arguments.")
-	elif length == 1:
-		print("1 argument:")
-		print("{}: {}".format(length, sys.argv[1]))
-	else:
-		print("{} arguments:".format(length))
-		for i in range(1, (length + 1)):
-			print("{}: {}".format(i, sys.argv[i]))
+    import sys
+    length = len(sys.argv)
+    length = length - 1
+    if length == 0:
+        print("0 arguments.")
+    elif length == 1:
+        print("1 argument:")
+        print("{}: {}".format(length, sys.argv[1]))
+    else:
+        print("{} arguments:".format(length))
+        for i in range(1, (length + 1)):
+            print("{}: {}".format(i, sys.argv[i]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-	import sys
-	length = len(sys.argv)
-	sum = 0
-	if length == 1:
-		print("{}".format(sum))
-	else:
-		for i in range(1, length):
-			sum += int(sys.argv[i])
-		print("{}".format(sum))
+    import sys
+    length = len(sys.argv)
+    sum = 0
+    if length == 1:
+        print("{}".format(sum))
+    else:
+        for i in range(1, length):
+            sum += int(sys.argv[i])
+        print("{}".format(sum))
