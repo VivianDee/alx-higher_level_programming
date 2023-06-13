7ce5b30b866ead16d7a5c5eec61a55a7e3320d44
Vivian Dagbue <vdagbue@gmail.com>
2023-06-13 02:49:18 -0700

    Add: Advanced task 12

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,22 @@
+#include "Python.h"
+#include <stdio.h>
+
+/**
+  * print_python_list_info - A function that prints some basic
+  *info about Python lists
+  * @p: A pointer
+  */
+
+void print_python_list_info(PyObject *p)
+{
+	int i;
+	PyListObject *ll;
+
+	list = (PyListObject *)p;
+	printf("[*] Size of the Python List = %ld\n", list->ob_base.ob_size);
+	printf("[*] Allocated = %ld\n", list->allocated);
+	for (i = 0; i < list->ob_base.ob_size; i++)
+	{
+		printf("Element %d: %s\n", i, list->ob_item[i]->ob_type->tp_name);
+	}
+}
