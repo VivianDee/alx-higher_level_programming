eeb57e6a23ff44add2eeb3e4aeec9de8d3467983
Vivian Dagbue <vdagbue@gmail.com>
2023-06-14 08:03:16 -0700

    Fix: extreme cases

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if roman_string is None or roman_string == "":
+        return None
     roman = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     s_case = {'IV': -2, 'IX': -2, 'XL': -20, 'XC': -20, 'CD': -200, 'CM': -200}
     decimal = sum(roman[character] for character in roman_string)
