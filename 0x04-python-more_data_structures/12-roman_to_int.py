91396964da71da3013135f49f3b9289445008f57
Vivian Dagbue <vdagbue@gmail.com>
2023-06-14 17:23:20 -0700

    Fix: If statement

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,7 +5,7 @@ def roman_to_int(roman_string):
     roman = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     s_case = {'IV': -2, 'IX': -2, 'XL': -20, 'XC': -20, 'CD': -200, 'CM': -200}
     for i in roman_string:
-        if i not in roman
+        if i not in roman:
             return 0
     decimal = sum(roman[character] for character in roman_string)
     length = len(roman_string)
