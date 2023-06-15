219128a1589f8ef720ec09bec36cdc3150e9932b
Vivian Dagbue <vdagbue@gmail.com>
2023-06-14 17:16:53 -0700

    Fix: Sorted keys in alphabetical order

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+
 def print_sorted_dictionary(a_dictionary):
     for key in sorted(list(a_dictionary.keys())):
-        print("{} : {}".format(key, a_dictionary.get(key)))
+        value = a_dictionary.get(key)
+        print("{} : {}".format(key, value))
