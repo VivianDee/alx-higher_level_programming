fde36bd94d257ce519593fd4ec21c6cd98d1e7da
Vivian Dagbue <vdagbue@gmail.com>
2023-06-14 17:40:15 -0700

    Test: Did not sort keys of dictionary inside the main dictionary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
-    for key in sorted(list(a_dictionary.keys())):
+    sorted_keys = list(a_dictionary.keys())
+    sorted_keys.sort()
+    for key in sorted_keys:
         value = a_dictionary.get(key)
         print("{:s} : {}".format(key, value))
