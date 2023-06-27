fc5734edd8dac0a6a4df48e178fc420967e644e2
Vivian Dagbue <vdagbue@gmail.com>
2023-06-27 12:46:54 -0700

    Add: Task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""The class Square"""
+
+
+class Square:
+    """Initialize the class with a size.
+
+        Args:
+            size (int, optional): The size of the object. Defaults to 0.
+
+        Raises:
+            TypeError: If the size is not an integer.
+            ValueError: If the size is less than 0.
+        """
+    def __init__(self, size=0):
+        if isinstance(size, int):
+            self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    @property
+    def size(self):
+        """Returns the private instance size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the private instance size to value"""
+        if isinstance(value, int):
+            self.__size = value
+        else:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Returns the area of the square"""
+        return self.__size ** 2
