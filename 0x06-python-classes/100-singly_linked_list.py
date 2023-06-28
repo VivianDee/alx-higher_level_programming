2c952b84b788be4711524b0268084e09a3a8f8e2
Vivian Dagbue <vdagbue@gmail.com>
2023-06-27 21:59:38 -0700

    Advanced task 1

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python3
+"""The class Node defines a node of a singly linked list"""
+
+
+class Node:
+    """Initialize the class with data and next_node.
+
+        Args:
+            data (int): The data of the object. Defaults to 0.
+            next_node (int): The next node of the object. Defaults to 0.
+        """
+    def __init__(self, data=0, next_node=None):
+            self.__data = data
+            self.__next_node = next_node
+
+
+    @property
+    def data(self):
+        """Returns the private instance data"""
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        """Sets the private instance data to value
+
+            Args:
+                value (int): The new data of the square
+
+        """
+        if isinstance(value, int) or value is None:
+            self.__data = value
+        else:
+            raise TypeError("data must be an integer")
+
+    @property
+    def next_node(self):
+        """Returns the private instance next_node"""
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        """Sets the private instance next_node to value
+
+            Args:
+                value (int): The new next_node of the square
+
+        """
+        if isinstance(value, Node) or value is None:
+            self.__next_node = value
+        else:
+            raise TypeError("next_node must be a Node object")
+
+
+
+"""The class SinglyLinkedList defines a singly linked list"""
+
+class SinglyLinkedList:
+    """Class SinglyLinkedList creates the linked list.
+
+        Args: There are no arguments
+    """
+
+    def __init__(self):
+        """Initialize the class with head."""
+        self.__head = None
+
+    def sorted_insert(self, value):
+        """sorted_insert inserts nodes in the linked list"""
+        node = Node(value)
+
+        if self.__head is None:
+            node.next_node = self.__head
+            self.__head = node
+        else:
+            current = self.__head
+            while current.next_node is not None:
+                current = current.next_node
+
+            current.next_node = node
+
+    def __str__(self):
+        """__str__ prints out the linked list"""
+        list = ""
+        head = self.__head
+        while head.next_node is not None:
+            list += str(head.data)
+            list += "\n"
+            head = head.next_node
+        return list
