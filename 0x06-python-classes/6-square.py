8cbfa8cee7aab740477b517045d85b69ca9691bb
Vivian Dagbue <vdagbue@gmail.com>
2023-06-28 17:07:02 +0100

    style: Pycodestyle error

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -15,13 +15,12 @@ class Square:
     def __init__(self, size=0, position=(0, 0)):
         if isinstance(size, int):
             self.__size = size
-            self.__position = position
         else:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        if isinstance(position, tuple) and len(value) == 2 and \
-                int(value[0]) > 0 and int(value[1]) > 0:
+        if isinstance(position, tuple) and len(position) == 2 and \
+                int(position[0]) >= 0 and int(position[1]) >= 0:
             self.__position = position
         else:
             raise TypeError("position must be a tuple of 2 positive integers")
