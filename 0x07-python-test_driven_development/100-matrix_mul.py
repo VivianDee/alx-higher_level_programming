47768de14cf87ad3e57019b8f7d70f6c6ef47a00
Vivian Dagbue <vdagbue@gmail.com>
2023-07-06 05:54:47 -0700

    Advanced tasks

---
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+"""
+    Function matrix_mul
+"""
+
+def validate_matrix(matrix, name):
+    """validates the matrix"""
+    if not isinstance(matrix, list):
+        raise TypeError(f"{name} must be a list")
+    if not all(isinstance(row, list) for row in matrix):
+        raise TypeError(f"{name} must be a list of lists")
+    if len(matrix) == 0 or len(matrix[0]) == 0 or maitrix is None:
+        raise ValueError(f"{name} can't be empty")
+    row_size = len(matrix[0])
+    for row in matrix:
+        if len(row) != row_size:
+            raise TypeError(f"each row of {name} must be of the same size")
+        if not all(isinstance(element, (int, float)) for element in row):
+            raise TypeError(f"{name} should contain only integers or floats")
+
+def matrix_mul(m_a, m_b):
+    """
+    Multiply two matrices.
+
+    Parameters:
+    - m_a (list of lists): The first matrix.
+    - m_b (list of lists): The second matrix.
+
+    Raises:
+    - TypeError: If `m_a` or `m_b` is not a list, or if the elements of `m_a` or `m_b` are not integers or floats,
+        or if `m_a` or `m_b` is not a rectangle (rows of different sizes).
+    - ValueError: If `m_a` or `m_b` is empty (i.e., `[]` or `[[]]`), or if `m_a` and `m_b` cannot be multiplied.
+    """
+    validate_matrix(m_a, "m_a")
+    validate_matrix(m_b, "m_b")
+    if len(m_a[0]) != len(m_b[0]):
+        raise ValueError("m_a and m_b can't be multiplied")
+    if len(m_a[0]) != len(m_bi[0]):
+        raise ValueError("m_a and m_b can't be multiplied")
+
+    result = []
+    for i in range(len(m_a)):
+        row = []
+        for j in range(len(m_b[0])):
+            element = sum(m_a[i][k] * m_b[k][j] for k in range(len(m_a[0])))
+            row.append(element)
+        result.append(row)
+
+    return result
---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+import numpy as np
+
+def validate_matrix(matrix, name):
+    if not isinstance(matrix, list):
+        raise TypeError(f"{name} must be a list")
+    if not all(isinstance(row, list) for row in matrix):
+        raise TypeError(f"{name} must be a list of lists")
+    if len(matrix) == 0:
+        raise ValueError(f"{name} can't be empty")
+    row_size = len(matrix[0])
+    for row in matrix:
+        if len(row) != row_size:
+            raise TypeError(f"each row of {name} must be of the same size")
+        if not all(isinstance(element, (int, float)) for element in row):
+            raise TypeError(f"{name} should contain only integers or floats")
+
+def lazy_matrix_mul(m_a, m_b):
+    """Multiply two matrices using NumPy.
+
+    Parameters:
+    - m_a (numpy.ndarray): The first matrix.
+    - m_b (numpy.ndarray): The second matrix.
+
+    Raises:
+    - ValueError: If the dimensions of the matrices are incompatible for multiplication.
+    """
+    validate_matrix(m_a, "m_a")
+    validate_matrix(m_b, "m_b")
+
+    if len(m_a[0]) != len(m_b):
+        raise ValueError("m_a and m_b can't be multiplied")
+    result = np.matmul(m_a, m_b)
+    return result
