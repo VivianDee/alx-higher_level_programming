5935ff8d5e20e000021a45e347859366980aff12
Vivian Dagbue <vdagbue@gmail.com>
2023-07-06 06:28:07 -0700

    Fix: return type

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -3,6 +3,7 @@
 import numpy as np
 
 def validate_matrix(matrix, name):
+    """validates the matrix arguments"""
     if not isinstance(matrix, list):
         raise TypeError(f"{name} must be a list")
     if not all(isinstance(row, list) for row in matrix):
@@ -29,10 +30,8 @@ def lazy_matrix_mul(m_a, m_b):
     validate_matrix(m_a, "m_a")
     validate_matrix(m_b, "m_b")
 
-    if len(m_a) != len(m_b):
-        raise ValueError("m_a and m_b can't be multiplied")
 
     if len(m_a[0]) != len(m_b[0]):
         raise ValueError("m_a and m_b can't be multiplied")
     result = np.matmul(m_a, m_b)
-    return result
+    return result.tolist()
