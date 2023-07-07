d807b7a235388ca4e9dcea0fe183f2161f5fe053
Vivian Dagbue <vdagbue@gmail.com>
2023-07-07 06:36:13 -0700

    Fix: Check after allocating memory

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,11 +9,16 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_table_t));
+	hash_table_t *table = NULL;
 	unsigned long int i;
 
-	table->size = size;
+	table = (hash_table_t *)malloc(sizeof(hash_table_t));
+	if (!table)
+		return (NULL);
+	table->size = (int)size;
 	table->array = (hash_node_t **)calloc(table->size, sizeof(hash_node_t));
+	if (!table->array)
+		return (NULL);
 
 	for (i = 0; i < table->size; i++)
 		table->array[i] = NULL;
--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,12 +11,23 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *item = (hash_node_t *)malloc(sizeof(hash_node_t));
-	unsigned long int index = key_index((const unsigned char *)key, 1024);
+	hash_node_t *item = NULL;
+	unsigned long int index = 0;
 
+	item = (hash_node_t *)malloc(sizeof(hash_node_t));
+	if (!item)
+		return (0);
+	if (!key)
+		return (0);
+	index = key_index((const unsigned char *)key, 1024);
 	ht->size = 1024;
 	item->key = (char *)malloc(strlen(key) + 1);
+	if (!item->key)
+		return (0);
+
 	item->value = (char *)malloc(strlen(value) + 1);
+	if (!item->value)
+		return (0);
 	strcpy(item->key, key);
 	strcpy(item->value, value);
 
