14d36f43a3bd3161f52e563777fef3f5acc6ff84
Vivian Dagbue <vdagbue@gmail.com>
2023-09-11 06:56:16 -0700

    Fix: Single argumrnt

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,6 +3,8 @@ const args = process.argv;
 const len = args.length;
 if (len < 3) {
   console.log('No argument');
+} else if (len < 4) {
+  console.log('Argument found');
 } else {
   console.log('Arguments found');
 }
