ba0381fcbc2f95d61a79a7dd4a35da4e9df1bf3c
Vivian Dagbue <vdagbue@gmail.com>
2023-09-11 08:28:29 -0700

    Tasks 6 to 10

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const args = process.argv;
-if (typeof args[2] === "undefined") {
+if (typeof args[2] === 'undefined') {
   console.log('No argument');
 } else {
   console.log(args[2]);
---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,3 @@
+#!/usr/bin/node
+const num = parseInt(process.argv[2]) + parseInt(process.argv[3]);
+console.log(num);
