1167cb139de54d8740bb8ff0643247d5708dc66e
Vivian Dagbue <vdagbue@gmail.com>
2023-09-11 08:58:33 -0700

    Fix: edgecase

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,9 +4,11 @@ const len = args.length;
 let high = 0;
 let low = 0;
 for (let i = 2; i < len; i++) {
-	if (parseInt(args[i]) > high) {
-		low = high;
-		high = parseInt(args[i]);
-	}
+  if (parseInt(args[i]) > high) {
+    low = high;
+    high = parseInt(args[i]);
+  } else if (parseInt(args[i]) > low && parseInt(args[i]) < high) {
+	  low = parseInt(args[i]);
+  }
 }
 console.log(low);
