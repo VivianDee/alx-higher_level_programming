56e92185b4412d17b32c18c08f3af5842cf91434
Vivian Dagbue <vdagbue@gmail.com>
2023-09-13 06:41:30 -0700

    Fix: Code style

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const myList = require('./100-data').list;
-const myArray = myList.map( (element, index) => {
-    return element * index;
+const myArray = myList.map((element, index) => {
+  return element * index;
 });
 console.log(myList);
 console.log(myArray);
--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
-const dict = require('./101-data').dict
+const dict = require('./101-data').dict;
 const keys = Object.keys(dict);
 const values = Object.values(dict);
-let newDict = {}
+const newDict = {};
 let len = keys.length - 1;
 while (len > -1) {
-    newDict[values[len]] = [];
-    len--;
+  newDict[values[len]] = [];
+  len--;
 }
 len = values.length - 1;
 while (len > -1) {
-    newDict[values[len]].push(keys[len])
-    len--;
+  newDict[values[len]].push(keys[len]);
+  len--;
 }
 console.log(newDict);
