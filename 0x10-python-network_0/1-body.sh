bf5fa30e038e29f8823327616d3e0f54e2831a86
Dagbue Vivian <vdagbue@gmail.com>
2023-09-28 07:11:32 -0700

    Fix: Algorithm

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # This script takes in a URL, sends a GET request to the URL, and displays the body of the response
-status=$(curl -sI "$1" | grep "HTTP" | cut -d " " -f 2)
-if [[ $status -eq 200 ]]; then
-	curl -sL "$1"
-fi
+curl -sL "$1"
