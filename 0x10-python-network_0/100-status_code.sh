3df526ae72569949b900e6685768c6e73fd97dfc
Dagbue Vivian <vdagbue@gmail.com>
2023-09-28 09:02:20 -0700

    Fix: Specification for each task

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -sI "$1" | grep "HTTP" | tail -n 1 | cut -d " " -f 2
+curl -slI -o /dev/null -w "%{http_code}" "$1"
--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 # This script that sends a JSON POST request to a URL
-if [ -f "$2" ]; then
-	check=$(cat "$2" | json_pp -t null 2>/dev/null)
-        if [ $? -eq 0 ]; then
-                curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1"
-	else
-		echo "Not a valid JSON"
-        fi
-fi
+curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1"
