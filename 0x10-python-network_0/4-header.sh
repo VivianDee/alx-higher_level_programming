d7aeeb1e33cdb1f2c6fe126292410ecf56acedc8
Dagbue Vivian <vdagbue@gmail.com>
2023-09-28 07:20:22 -0700

    Fix: Synthax error

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -sH "X-School-User-Id : 89" "$1"
+curl -sH "X-School-User-Id: 89" "$1"
